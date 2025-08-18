6df2fcad6352fe47d669f6f8cfccc34c58317b95
91708 <2571sharmashivam@gmail.com>
2025-08-18 22:10:39 +0530

    18-Aug-2025/22:10

---
+++ C#/ThreadsChapter/ThreadsChapter/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Threading;
+
+/*
+Console.WriteLine("Hello World 1");
+Thread.Sleep(1000);
+Console.WriteLine("Hello World 2");
+Thread.Sleep(1000);
+Console.WriteLine("Hello World 3");
+Thread.Sleep(1000);
+Console.WriteLine("Hello World 4");
+Thread.Sleep(1000);
+Console.WriteLine("Hello World 5");
+*/
+
+/*
+new Thread(() => 
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("Thread 1");
+}).Start();
+new Thread(() => 
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("Thread 2");
+}).Start();
+new Thread(() => 
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("Thread 3");
+}).Start();
+new Thread(() => 
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("Thread 4");
+}).Start();
+new Thread(() => 
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("Thread 5");
+}).Start();
+*/
+
+
+/*
+  Enumerable.Range(1, 1000).ToList().ForEach(f =>
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " started");
+                Thread.Sleep(1000);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " finished");
+
+            });
+        });
+        Console.ReadLine();
+ */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Main Thread Started");
+        Thread thread1 = new Thread(Thread1Function);
+        Thread thread2 = new Thread(Thread2Function);
+        thread1.Start();
+        thread2.Start();
+
+        //thread1.Join();
+        //Console.WriteLine("Thread1Function done");
+        if (thread1.Join(1000))
+        {
+            Console.WriteLine("Thread1Function done");
+        }
+        else
+        {
+            Console.WriteLine("Thread1Function wasn't done within 1 second.");
+        }
+        thread2.Join();
+        Console.WriteLine("Thread2Function done");
+
+        if (thread1.IsAlive)
+        {
+            Console.WriteLine("Thread is still doing stuff.");
+        }
+        else
+        {
+            Console.WriteLine("Thread one is done.");
+        }
+
+        Console.WriteLine("Main Thread Ended");
+    }
+
+    public static void Thread1Function()
+    {
+        Console.WriteLine("Thread1Function started");
+        Thread.Sleep(3000);
+        Console.WriteLine("Thread1Function coming back to Main");
+    }
+    public static void Thread2Function()
+    {
+        Console.WriteLine("Thread2Function started");
+    }
+}
---
+++ C#/WpfTasks/WpfTasks/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTasks
+{
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+            "Html",
+            typeof(string),
+            typeof(MainWindow),
+            new FrameworkPropertyMetadata(OnHtmlChanged)
+            );
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                Debug.WriteLine($"Thread No. : {Thread.CurrentThread.ManagedThreadId}");
+                HttpClient client = new HttpClient();
+                string html = client.GetStringAsync("http://speedtest.tele2.net/20MB.zip").Result;
+                MessageBox.Show(html);
+
+                MyButton.Dispatcher.Invoke(() => MyButton.Content = "Done");
+
+            });
+
+        }
+
+        private async void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            string myHtml = "ABC";
+            await Task.Run(() =>
+            {
+                HttpClient client = new HttpClient();
+                string html = client.GetStringAsync("https://youtube.com").Result;
+                myHtml = html;
+
+            });
+            MyButton.Content = "Done Downloading";
+            MyWebBrowser.SetValue(HtmlProperty, myHtml);
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser webBrowser = (WebBrowser)dependencyObject;
+            if (webBrowser != null)
+            {
+                webBrowser.NavigateToString(e.NewValue as string);
+            }
+        }
+
+    }
+}
